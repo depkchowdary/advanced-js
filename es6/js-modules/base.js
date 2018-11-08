fa3dd261be59b8978cf4ce994eedbe91ddb5061c
Tatineni <Deepak.Tatineni@mastercard.com>
2018-11-08 20:40:44 +0530

    CSS practice

--- es6/js-modules/base.js
+++ es6/js-modules/base.js
@@ -1,9 +1,16 @@
 'use strict'
 
-import {projectId, projectName} from './module-1.js';
+import project from './module-1.js';
 
-import {deafult as myProjectName , myProjectId}  from './module-2.js'
+project = {
+    projectId: 8000
+}
+
+console.log(project)
+
+
+/* import {deafult as myProjectName , myProjectId}  from './module-2.js'
 
 console.log("in base js")
 console.log(`${projectId} ${projectName}`)
-console.log(`${myProjectId} ${myProjectName}`)
+console.log(`${myProjectId} ${myProjectName}`) */
--- es6/js-modules/module-1.js
+++ es6/js-modules/module-1.js
@@ -1,3 +1,8 @@
-export let projectId = 99
+/* export let projectId = 99
 export let projectName = "VS Code"
-//any number of objects can be exported
+//any number of objects can be exported */
+
+
+export let project = {
+    projectId : 99
+}
